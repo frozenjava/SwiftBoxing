3ab2f84e20795028a569495466036d20c1627747
frozenjava <newbi3@innoc-sec.net>
2015-06-15 00:21:13 -0400

    Initial Commit

---
+++ CLIBoxing/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CLIBoxing
+//
+//  Created by Joshua Artuso on 6/15/15.
+//  Copyright (c) 2015 n3rdcav3. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
